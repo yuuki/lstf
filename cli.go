f8e67d6ff15c8a077e7b5589b6af1b39df08ccc6
y_uuki <yuki.tsubo@gmail.com>
2018-03-04 22:24:21 +0900

    Add draft version

--- cli.go
+++ cli.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"text/tabwriter"
+
+	"github.com/yuuki/lstf/tcpflow"
 )
 
 const (
@@ -42,7 +45,26 @@ func (c *CLI) Run(args []string) int {
 		return exitCodeOK
 	}
 
+	flows, err := tcpflow.GetHostFlows()
+	if err != nil {
+		log.Printf("failed to get host flows: %v", err)
+		return exitCodeErr
+	}
+
+	c.PrintHostFlows(flows)
+
 	return exitCodeOK
+}
+
+// PrintHostFlows prints the host flows.
+func (c *CLI) PrintHostFlows(flows tcpflow.HostFlows) {
+	// Format in tab-separated columns with a tab stop of 8.
+	tw := tabwriter.NewWriter(c.outStream, 0, 8, 0, '\t', 0)
+	fmt.Fprintln(tw, "Local Address:Port\t <--> \tPeer Address:Port")
+	for _, flow := range flows {
+		fmt.Fprintf(tw, "%s\n", flow)
+	}
+	tw.Flush()
 }
 
 var helpText = `Usage: lstf [options]
---
+++ tcpflow/tcpflow.go
@@ -0,0 +1,109 @@
+package tcpflow
+
+import (
+	"fmt"
+	"net"
+
+	gnet "github.com/shirou/gopsutil/net"
+	"github.com/yuuki/lstf/netutil"
+)
+
+// FlowDirection are bitmask that represents both Active or Passive.
+type FlowDirection int
+
+const (
+	// FlowUnknown are unknown flow.
+	FlowUnknown FlowDirection = 1 << iota
+	// FlowActive are 'active open'.
+	FlowActive
+	// FlowPassive are 'passive open'
+	FlowPassive
+)
+
+// AddrPort are <addr>:<port>
+type AddrPort struct {
+	Addr string `json:"addr"`
+	Port string `json:"port"`
+}
+
+// String returns the string representation of the AddrPort.
+func (a *AddrPort) String() string {
+	return net.JoinHostPort(a.Addr, a.Port)
+}
+
+// HostFlow represents a `host flow`.
+type HostFlow struct {
+	Direction FlowDirection
+	Local     *AddrPort `json:"local"`
+	Peer      *AddrPort `json:"peer"`
+}
+
+// String returns the string representation of HostFlow.
+func (f *HostFlow) String() string {
+	switch f.Direction {
+	case FlowActive:
+		return fmt.Sprintf("%s\t --> \t%s", f.Local, f.Peer)
+	case FlowPassive:
+		return fmt.Sprintf("%s\t <-- \t%s", f.Local, f.Peer)
+	}
+	return ""
+}
+
+// UniqKey returns the unique key for connections aggregation
+func (f *HostFlow) UniqKey() string {
+	return fmt.Sprintf("%d-%s-%s", f.Direction, f.Local, f.Peer)
+}
+
+// HostFlows represents a group of host flow by unique key.
+type HostFlows map[string]*HostFlow
+
+func (hf HostFlows) insert(flow *HostFlow) {
+	key := flow.UniqKey()
+	if _, ok := hf[key]; !ok {
+		hf[key] = flow
+		return
+	}
+	return
+}
+
+func GetHostFlows() (HostFlows, error) {
+	ports, err := netutil.LocalListeningPorts()
+	if err != nil {
+		return nil, err
+	}
+	conns, err := gnet.Connections("tcp")
+	if err != nil {
+		return nil, err
+	}
+	flows := HostFlows{}
+	for _, conn := range conns {
+		if conn.Status == "LISTEN" {
+			continue
+		}
+		lport := fmt.Sprintf("%d", conn.Laddr.Port)
+		rport := fmt.Sprintf("%d", conn.Raddr.Port)
+		if contains(ports, lport) {
+			flows.insert(&HostFlow{
+				Direction: FlowPassive,
+				Local:     &AddrPort{Addr: "localhost", Port: lport},
+				Peer:      &AddrPort{Addr: conn.Raddr.IP, Port: "many"},
+			})
+		} else {
+			flows.insert(&HostFlow{
+				Direction: FlowActive,
+				Local:     &AddrPort{Addr: "localhost", Port: "many"},
+				Peer:      &AddrPort{Addr: conn.Raddr.IP, Port: rport},
+			})
+		}
+	}
+	return flows, nil
+}
+
+func contains(strs []string, s string) bool {
+	for _, str := range strs {
+		if str == s {
+			return true
+		}
+	}
+	return false
+}
