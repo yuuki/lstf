dac309215dced948b376ea25094d8312747ae9f4
y_uuki <yuki.tsubo@gmail.com>
2018-06-16 15:53:45 +0900

    Add testing for LoadListeningPorts

--- netutil/netutil_test.go
+++ netutil/netutil_test.go
@@ -1,6 +1,8 @@
 package netutil
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestLocalIPAddrss(t *testing.T) {
 	addrs, err := LocalIPAddrs()
@@ -8,6 +10,16 @@ func TestLocalIPAddrss(t *testing.T) {
 		t.Fatalf("should not raise error: %v", err)
 	}
 	if len(addrs) == 0 {
+		t.Error("localIPAddrs() should not be len == 0")
+	}
+}
+
+func TestLocalListeningPorts(t *testing.T) {
+	ports, err := LocalListeningPorts()
+	if err != nil {
+		t.Fatalf("should not raise error: %v", err)
+	}
+	if len(ports) == 0 {
 		t.Error("localIPAddrs() should not be len == 0")
 	}
 }
