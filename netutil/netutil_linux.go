25ca5503fed34c9c61e5fb9d6887d45dda132ee3
y_uuki <yuki.tsubo@gmail.com>
2018-06-19 04:50:52 +0900

    Improve performance by removing the inode and pid process

--- netutil/netutil_linux.go
+++ netutil/netutil_linux.go
@@ -3,9 +3,18 @@
 package netutil
 
 import (
+	"bytes"
+	"encoding/hex"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"strconv"
+	"strings"
 
 	"github.com/elastic/gosigar/sys/linux"
+
+	gnet "github.com/shirou/gopsutil/net"
 )
 
 // NetlinkConnections returns connection stats.
@@ -45,6 +54,105 @@ func NetlinkLocalListeningPorts() ([]string, error) {
 			continue
 		}
 		ports = append(ports, fmt.Sprintf("%v", diag.SrcPort()))
+	}
+	return ports, nil
+}
+
+const (
+	tcpProcFilename = "/proc/net/tcp"
+)
+
+// Addr is <addr>:<port>.
+type Addr struct {
+	IP   string `json:"ip"`
+	Port uint32 `json:"port"`
+}
+
+// ConnectionStat represents staticstics for a connection.
+type ConnectionStat struct {
+	Laddr  Addr
+	Raddr  Addr
+	Status linux.TCPState
+}
+
+// ProcfsConnections returns connection stats.
+// ref. https://github.com/shirou/gopsutil/blob/c23bcca55e77b8389d84b09db8c5ac2b472070ef/net/net_linux.go#L656
+func ProcfsConnections() ([]*ConnectionStat, error) {
+	body, err := ioutil.ReadFile(tcpProcFilename)
+	if err != nil {
+		return nil, err
+	}
+
+	lines := bytes.Split(body, []byte("\n"))
+	conns := make([]*ConnectionStat, 0, len(lines)-1)
+	for _, line := range lines[1:] {
+		l := strings.Fields(string(line))
+		if len(l) < 10 {
+			continue
+		}
+		laddr := l[1]
+		raddr := l[2]
+		status, err := strconv.ParseUint(l[3], 16, 8)
+		if err != nil {
+			log.Printf("decode error: %v", err)
+		}
+		la, err := decodeAddress(laddr)
+		if err != nil {
+			continue
+		}
+		ra, err := decodeAddress(raddr)
+		if err != nil {
+			continue
+		}
+
+		conns = append(conns, &ConnectionStat{
+			Laddr:  la,
+			Raddr:  ra,
+			Status: linux.TCPState(status),
+		})
+	}
+
+	return conns, nil
+}
+
+// decodeAddress decode addresse represents addr in proc/net/*
+// ex:
+// "0500000A:0016" -> "10.0.0.5", 22
+// "0085002452100113070057A13F025401:0035" -> "2400:8500:1301:1052:a157:7:154:23f", 53
+// ref. https://github.com/shirou/gopsutil/blob/c23bcca55e77b8389d84b09db8c5ac2b472070ef/net/net_linux.go#L600
+func decodeAddress(src string) (Addr, error) {
+	t := strings.Split(src, ":")
+	if len(t) != 2 {
+		return Addr{}, fmt.Errorf("does not contain port, %s", src)
+	}
+	addr := t[0]
+	port, err := strconv.ParseInt("0x"+t[1], 0, 64)
+	if err != nil {
+		return Addr{}, fmt.Errorf("invalid port, %s", src)
+	}
+	decoded, err := hex.DecodeString(addr)
+	if err != nil {
+		return Addr{}, fmt.Errorf("decode error, %s", err)
+	}
+	var ip net.IP
+	// Assumes this is little_endian
+	ip = net.IP(gnet.Reverse(decoded))
+	return Addr{
+		IP:   ip.String(),
+		Port: uint32(port),
+	}, nil
+}
+
+// FilterByLocalListeningPorts filters ConnectionStat slice by the local listening ports.
+func FilterByLocalListeningPorts(conns []*ConnectionStat) ([]string, error) {
+	ports := []string{}
+	for _, conn := range conns {
+		if conn.Status != linux.TCP_LISTEN {
+			continue
+		}
+		if conn.Laddr.IP == "0.0.0.0" || conn.Laddr.IP == "127.0.0.1" || conn.Laddr.IP == "::" {
+			ports = append(ports, fmt.Sprintf("%d", conn.Laddr.Port))
+		}
 	}
 	return ports, nil
 }
--- tcpflow/tcpflow_linux.go
+++ tcpflow/tcpflow_linux.go
@@ -52,3 +52,37 @@ func GetHostFlowsByNetlink() (HostFlows, error) {
 	}
 	return flows, nil
 }
+
+// GetHostFlowsByProcfs gets host flows from procfs.
+func GetHostFlowsByProcfs() (HostFlows, error) {
+	conns, err := netutil.ProcfsConnections()
+	if err != nil {
+		return nil, err
+	}
+	ports, err := netutil.FilterByLocalListeningPorts(conns)
+	if err != nil {
+		return nil, err
+	}
+	flows := HostFlows{}
+	for _, conn := range conns {
+		if conn.Status == linux.TCP_LISTEN {
+			continue
+		}
+		lport := fmt.Sprintf("%d", conn.Laddr.Port)
+		rport := fmt.Sprintf("%d", conn.Raddr.Port)
+		if contains(ports, lport) {
+			flows.insert(&HostFlow{
+				Direction: FlowPassive,
+				Local:     &AddrPort{Addr: conn.Laddr.IP, Port: lport},
+				Peer:      &AddrPort{Addr: conn.Raddr.IP, Port: "many"},
+			})
+		} else {
+			flows.insert(&HostFlow{
+				Direction: FlowActive,
+				Local:     &AddrPort{Addr: conn.Laddr.IP, Port: "many"},
+				Peer:      &AddrPort{Addr: conn.Raddr.IP, Port: rport},
+			})
+		}
+	}
+	return flows, nil
+}
