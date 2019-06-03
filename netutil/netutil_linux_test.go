a1cce3469a938a31dc7dbcca7b3fbed91d8af5fb
y_uuki <yuki.tsubo@gmail.com>
2019-06-03 14:01:32 +0900

    Add parseProcStat tetsing

--- netutil/netutil_linux_test.go
+++ netutil/netutil_linux_test.go
@@ -2,7 +2,11 @@
 
 package netutil
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
 
 func TestNetlinkConnections(t *testing.T) {
 	conns, err := NetlinkConnections()
@@ -11,5 +15,20 @@ func TestNetlinkConnections(t *testing.T) {
 	}
 	if len(conns) == 0 {
 		t.Error("NetlinkConnections() should not be len == 0")
+	}
+}
+
+func TestParseProcStat(t *testing.T) {
+	cur, _ := os.Getwd()
+	root := filepath.Join(cur, "../testdata")
+	pid := 10000
+
+	pname, err := parseProcStat(root, pid)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if pname != "nginx" {
+		t.Errorf("process name '%s' should be 'nginx'", pname)
 	}
 }
